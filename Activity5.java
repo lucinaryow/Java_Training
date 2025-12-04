482d23e90a925c318b8e8889ae8ca78baaca1d02
Mel Joseph Q. Lucinario <mjqlucinario@headoffice.corp.bpi.com.ph>
2025-12-04 16:52:36 +0800

    Upload activity 5

---
+++ Activity5.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package Day_1;
+
+/**
+ * 
+ */
+
+
+
+
+public class Activity5 {
+    public static void main(String[] args) {
+    	
+    activity5();	
+    	
+    }
+    
+    static void activity5() {
+
+    	int sum = 0;
+
+        // Loop from 1 to 50
+        for (int i = 1; i <= 50; i++) {
+            sum += i; // Add each number to sum
+        }
+
+        // Display the result
+        System.out.println("Sum = " + sum);
+        System.out.println("Neil napakagwapo mo");
+    }
+
+}
+
+
+
+
