9b37ed790eb7fc40259147228b99d0458a0ea069
Mel Lucinario <128769496+lucinaryow@users.noreply.github.com>
2026-01-14 16:19:06 +0800

    Activity6

---
+++ InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package M4_Activity6;
+
+public class InsufficientFundsException extends Exception {
+
+	public InsufficientFundsException(String message) {
+			super(message);
+	}
+}
---
+++ InvalidAmountException.java
@@ -0,0 +1,8 @@
+package M4_Activity6;
+
+public class InvalidAmountException extends Exception {
+	
+	public InvalidAmountException(String message) {
+			super(message);
+	}
+}
